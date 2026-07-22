f6d9b42f5974531100c2ede3370b70ef79204723
PAPAJI-ADMIN <brosajid34@gmail.com>
2026-07-22 21:08:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,8 +18,8 @@ SMM_API_URL = "https://smmfurious.com/api/v2"
 SMM_API_KEY = "21a356d044975d8013d5fe0fda04891fb307d7ac"
 
 # --- SERVICE SETTINGS (FAST & CHEAP) ---
-# ID 26585: Telegram Post Views - Instant Start⚡ (₹30/1k)
-SMM_SERVICE_ID = "26585" 
+# ID 28108: Telegram Post Views - Instant Start⚡ (₹30/1k)
+SMM_SERVICE_ID = "28108" 
 
 # --- BUSINESS PRICING ---
 PRICE_PER_1000 = 30.0 # Selling price (100 views = ₹5)
